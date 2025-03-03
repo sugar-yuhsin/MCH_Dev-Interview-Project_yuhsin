4b57b2bd5228073bd12e01fad3cd2cd9c06474b8
iamappleshih@gmail.com <iamappleshih@gmail.com>
2025-03-03 16:39:25 +0900

    fix_

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,5 @@ import vue from "@vitejs/plugin-vue";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/MCH_Dev-Interview-Project/',
+  base: '/MCH_Dev-Interview-Project_yuhsin/',
 });
